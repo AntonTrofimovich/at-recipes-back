ebb06afdc427ec4f50e5177e65739ae3828181a1
Anton Trafimovich <antont@compatibl.com>
2021-01-03 14:42:11 +0300

    In prorgess.

--- db/index.js
+++ db/index.js
@@ -11,6 +11,6 @@ const pgp = pgPromise(initOptions);
 
 const db = pgp("postgres://root:secret@db:5432/recipes");
 
-db.recipes.create();
+db.recipes.create().catch(e => console.log(e))
 
 module.exports = { db, pgp };
